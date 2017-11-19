d3a82718a01712e5f1d5b421d750f0fa24a80b7e
Mikhail Grinfeld <grinfeld@gmail.com>
2017-11-19 06:11:31 +0200

    1. added @bean to get http and smpp configurations 2. added current configuration files into resources

--- src/main/java/com/telemessage/tools/simulator/config/AppConfig.java
+++ src/main/java/com/telemessage/tools/simulator/config/AppConfig.java
@@ -1,9 +1,44 @@
 package com.telemessage.tools.simulator.config;
 
+import com.telemessage.tools.simulator.model.conf.AbstractConnectionsConf;
+import com.telemessage.tools.simulator.model.conf.http.HttpConnectionsConf;
+import com.telemessage.tools.simulator.model.conf.smpp.SMPPConnectionsConf;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+
 /**
  * @author Grinfeld Mikhail
  * @since 11/6/2017.
  */
+@Configuration
 public class AppConfig {
+
+    public static class ConnectionConfiguration {
+        @Value("${com.telemessage.tools.simulator.conf.smpp}")
+        protected String smpps;
+        @Value("${com.telemessage.tools.simulator.conf.http}")
+        protected String https;
+
+        @Bean
+        public HttpConnectionsConf httpConf() throws Exception {
+            return loadConf(HttpConnectionsConf.class, https);
+        }
+
+        @Bean
+        public SMPPConnectionsConf smtpConf() throws Exception {
+            return loadConf(SMPPConnectionsConf.class, smpps);
+        }
+
+        private static <T extends AbstractConnectionsConf> T loadConf(Class<T> clazzT, String path) throws Exception {
+            JAXBContext context = JAXBContext.newInstance(clazzT);
+
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            return  (T) unmarshaller.unmarshal(ClassLoader.getSystemResourceAsStream(path));
+        }
+    }
 
 }
--- src/test/java/com/telemessage/tools/simulator/model/config/UnmarshallerTest.java
+++ src/test/java/com/telemessage/tools/simulator/model/config/UnmarshallerTest.java
@@ -1,14 +1,12 @@
-package com.telemessage.tools.simulator.model.conf;
+package com.telemessage.tools.simulator.model.config;
 
+import com.telemessage.tools.simulator.config.AppConfig;
 import com.telemessage.tools.simulator.model.conf.http.HttpConnectionConf;
 import com.telemessage.tools.simulator.model.conf.http.HttpConnectionsConf;
 import com.telemessage.tools.simulator.model.conf.smpp.SMPPConnectionConf;
 import com.telemessage.tools.simulator.model.conf.smpp.SMPPConnectionsConf;
 import org.junit.Test;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Unmarshaller;
-
+import org.powermock.reflect.Whitebox;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -23,11 +21,8 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 public class UnmarshallerTest {
 
     @Test
-    public void unmarshalSMPPConfTest() throws Exception {
-        JAXBContext context = JAXBContext.newInstance(SMPPConnectionsConf.class);
-
-        Unmarshaller unmarshaller = context.createUnmarshaller();
-        SMPPConnectionsConf c = (SMPPConnectionsConf) unmarshaller.unmarshal(ClassLoader.getSystemResourceAsStream("smpps.xml"));
+    public void loadConf_whenSmpp_ExpectedLoaded5Confs() throws Exception {
+        SMPPConnectionsConf c = Whitebox.invokeMethod(AppConfig.ConnectionConfiguration.class, "loadConf", SMPPConnectionsConf.class, "smpps.xml");
 
         assertThat(c).isNotNull();
         List<SMPPConnectionConf> connections = c.getConnections();
@@ -42,11 +37,8 @@ public class UnmarshallerTest {
     }
 
     @Test
-    public void unmarshalHttpConfTest() throws Exception {
-        JAXBContext context = JAXBContext.newInstance(HttpConnectionsConf.class);
-
-        Unmarshaller unmarshaller = context.createUnmarshaller();
-        HttpConnectionsConf c = (HttpConnectionsConf) unmarshaller.unmarshal(ClassLoader.getSystemResourceAsStream("https.xml"));
+    public void loadConf_whenHttp_ExpectedLoaded6Confs() throws Exception {
+        HttpConnectionsConf c = Whitebox.invokeMethod(AppConfig.ConnectionConfiguration.class, "loadConf", HttpConnectionsConf.class, "https.xml");
 
         assertThat(c).isNotNull();
         List<HttpConnectionConf> connections = c.getConnections();
